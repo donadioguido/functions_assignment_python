8c3e4a277e1453a59bcbd3765369027e6dcb6d6d
donadioguido <69935112+donadioguido@users.noreply.github.com>
2020-10-27 17:14:49 -0300

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,25 @@
+def title(titleOfSong):
+    Output = titleOfSong
+    return Output
+
+def songWriter(songWriterOfSong):
+    Output = songWriterOfSong
+    return Output
+
+def yearReleased(songReleasedIn):
+    Output = songReleasedIn
+    return Output
+
+def inCharts(numberOneInCharts):
+    Output = numberOneInCharts
+    return Output
+
+var1 = title("Your latest trick")
+var2 = songWriter("Mark Knopfler")
+var3 = yearReleased(1986)
+var4 = inCharts(False)
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
